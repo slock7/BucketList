54a7f0842e0fb04c1abb687c423d5fbe4ce84348
Raf <polar27@gmail.com>
2018-06-23 17:22:58 +0200

    Added EF functionality

---
+++ BucketList/Models/BucketListDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BucketList.Models
+{
+    public class BucketListDbContext : DbContext
+    {
+        public BucketListDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<BucketData> BucketDatas { get; set; }
+    }
+}
--- BucketList/Startup.cs
+++ BucketList/Startup.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BucketList.Models;
+using BucketList.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -14,13 +16,21 @@ namespace BucketList
 {
     public class Startup
     {
+        private IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IGreeter, Greeter>();
+            services.AddDbContext<BucketListDbContext>(options => options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc();
-            services.AddSingleton<IBucketData, InMemoryData>();
+            services.AddScoped<IBucketData, SqlBucketData>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
